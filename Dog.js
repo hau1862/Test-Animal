7de77a2a6f6773f9dd5b923de0c3897c2e284cc7
hau <hau1862@gmail.com>
2021-04-08 16:22:06 +0700

    Fix Eat Method in Dog

--- Dog.js
+++ Dog.js
@@ -1,9 +1,10 @@
 function Dog(name) {
     this.name = name;
+    this.stomach = [];
 }
 
-Dog.prototype.eat = function() {
-    console.log("eating");
+Dog.prototype.eat = function(cat) {
+    this.stomach.push(cat);
 }
 
 module.exports = Dog;
